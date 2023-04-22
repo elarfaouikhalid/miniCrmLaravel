7403cdad5feed3f01b48085619ff1f3548db16d9
elarfaouikhalid464 <elarfaouikhalid464@gmail.com>
2023-04-22 16:32:27 +0100

    changes

---
+++ app/Repositories/EmployeeRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+ 
+interface EmployeeRepositoryInterface
+{
+    public function create(array $data): User;
+    public function findByEmail(string $email): ?User;
+}
